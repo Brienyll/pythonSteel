0cfba14159457ef17753615f356028e0ce6c6568
Brienyll <brienyll29@gmail.com>
2018-03-22 23:53:37 -0700

    added bouncer, checks age if 21 or over

---
+++ bouncer.py
@@ -0,0 +1,8 @@
+age = input("How old are you? ")
+
+if int(age) >= 18 and int(age) <=21:
+    print("You can enter, but need a wristband!")
+elif int(age) >21:
+    print("You are good to enter and can drink!")
+else:
+    print("You can't come in, little one! :(")
