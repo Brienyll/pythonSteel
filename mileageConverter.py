68cf6b24d72feac8e6ed5518debab1a50933ba7f
Brienyll <brienyll29@gmail.com>
2018-03-27 19:51:51 -0700

    Update mileageConverter.py

--- mileageConverter.py
+++ mileageConverter.py
@@ -3,4 +3,4 @@ miles = input()
 kms = float(miles)*1.60934
 kms = round(kms, 2)
 #print("That is equal to " + str(kms) + " miles")
-print(f"Your {miles}mi is equal to {kms}km")
+print(f"Your { miles }mi is equal to { kms }km")
